9bbe30c312ecbb993c92c80dc8dae0dde7c09669
Baptiste Fontaine <batifon@yahoo.fr>
2013-12-27 17:29:12 +0100

    missing functions

--- src/sujet_search.ml
+++ src/sujet_search.ml
@@ -11,25 +11,24 @@ let lcons el fn =
   cons el (fn ())
 
 let get =
-  fun Stream(f) -> f ()
+  fun (Stream f) -> f ()
 
 let take s n =
-  if n <= 0 then []
-  else
-    let rec take_r s n acc =
+  let rec take_r s n acc =
+    if n <= 0 then acc
+    else
       let Stream(res) = s in
         match res () with
         | None -> acc
-        | Some(x, s') -> take_r s' (n - 1) x::acc
-    in List.reverse (take_r s n [])
+        | Some(x, s') -> take_r s' (n - 1) (x::acc)
+  in List.rev (take_r s n [])
 
 let number_stream =
-  (* infinite stream [0, 1, 2, 3, ...] *)
-  Stream(fun () -> None) (* TODO *)
-
-(*
+  let rec next_number n =
+    Stream(fun () -> Some (n, next_number (n+1)))
+  in
+    next_number 0
 
 let stream_of_list l =
-  TODO
+  List.fold_right (fun el st -> cons el st) l nil
 
-*)
